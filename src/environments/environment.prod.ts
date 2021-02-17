8901ddc6b2ae505a809087a48c0a28f21b2634ee
Biruk <mengi024@morris.umn.edu>
2021-02-17 02:38:07 -0600

    added dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyD6J81nNllbPJUG9MKySvzniSrJbnYs9CE",
+    authDomain: "api-parser-1e98e.firebaseapp.com",
+    databaseURL: "https://api-parser-1e98e-default-rtdb.firebaseio.com",
+    projectId: "api-parser-1e98e",
+    storageBucket: "api-parser-1e98e.appspot.com",
+    messagingSenderId: "859023123920",
+    appId: "1:859023123920:web:8ee9c81dabc2e228cb85d5",
+    measurementId: "G-KGF2DF3MN1"
+  }
 };
